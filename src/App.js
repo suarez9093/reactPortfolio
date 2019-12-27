ad89cfe33f39af8e621391f9c05248efeb65d1a3
Alex <suarez9093@hotmail.com>
2019-12-26 22:29:38 -0700

    set a CssBaseline, added a Container

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import React from 'react';
-import { Container, Row, Col } from "reactstrap";
-import NavbarComponent from "./components/Navbar";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container'
+// import NavbarComponent from "./components/Navbar";
 import Header from "./components/Header";
-import Portfolio from "./components/Portfolio";
-import Footer from "./components/Footer";
+// import Portfolio from "./components/Portfolio";
+// import Footer from "./components/Footer";
 import Button from '@material-ui/core/Button';
 import "./style.css";
 
 function App() {
   return (
-    <div>
-      <Container>
-              <Header/>
-        <Row>
-          <NavbarComponent/>
-        </Row>
-        <Row>
-          <Portfolio/>
-          <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-        </Row>
-        <Row>
-          <Footer/>
-        </Row>
-
-      </Container>
-    </div>
+    <React.Fragment>
+            <CssBaseline />
+            {/* Rest of application goes underneath the CssBaseline */}
+            <Container>
+              <Header />
+            </Container>
+    </React.Fragment>
   );
 }
 
