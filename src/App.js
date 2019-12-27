a6fef885e5077d0ea1a6602bb2c249ad48151e89
Alex <suarez9093@hotmail.com>
2019-12-26 21:40:48 -0700

    components made

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { Container, Row, Col } from "reactstrap";
 import NavbarComponent from "./components/Navbar";
 import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
 import "./style.css";
 
 function App() {
   return (
     <div>
       <Container>
-          
               <Header/>
-          
         <Row>
           <NavbarComponent/>
+        </Row>
+        <Row>
+          <Portfolio/>
+        </Row>
+        <Row>
+          <Footer/>
         </Row>
 
       </Container>
