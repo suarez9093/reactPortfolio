a8a2efce82aa5c5f839bccf37bfa77b8997cf05d
Alex <suarez9093@hotmail.com>
2019-12-28 15:48:38 -0700

    stashing

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -14,6 +14,7 @@ import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
 
 const drawerWidth = 240;
 const color = "red";
@@ -56,10 +57,10 @@ export default function NavbarComponent() {
       >
         <div className={classes.toolbar} />
         <List>
-            <ListItem><GitHubIcon/></ListItem>
-            <ListItem><LinkedInIcon/></ListItem>
+        <ListItem><PhoneIphoneIcon/></ListItem>
             <ListItem><MailIcon/></ListItem>
             <ListItem><GitHubIcon/></ListItem>
+            <ListItem><LinkedInIcon/></ListItem>
         </List>
       </Drawer>
     </div>
