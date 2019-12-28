bcd1c1488759010a9eed996ee60652f8460dd5b2
Alex <suarez9093@hotmail.com>
2019-12-28 15:46:29 -0700

    icons for social media platforms added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container'
-// import NavbarComponent from "./components/Navbar";
+import NavbarComponent from "./components/Navbar";
 import Header from "./components/Header";
 // import Portfolio from "./components/Portfolio";
 // import Footer from "./components/Footer";
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
             <CssBaseline />
             {/* Rest of application goes underneath the CssBaseline */}
             <Container>
+              <NavbarComponent/>
               <Header />
             </Container>
     </div>
