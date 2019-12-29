6a421c011c1c4b366462f443ad093f8ebf39eef3
Alex <suarez9093@hotmail.com>
2019-12-28 21:52:07 -0700

    added Grid and spacig to portfolio links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,30 +1,107 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+// import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
+import pic from "./img/dbz.png"
+import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(24),
-    },
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 145,
   },
-}));
+  media: {
+    height: 200,
+  },
+  img: {
+    backgroundColor: "blue"
+  }
+});
 
+const myStyles = {
+  img: {
+    // maxWidth: 100,
+    // maxHeight: 100,
+    backgroundPosition: 50
+  }
+}
 export default function Portfolio() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      {/* <Paper elevation={0} />
-      <Paper /> */}
-      <Paper elevation={3} />
-      <Paper elevation={3} />
-      <Paper elevation={3} />
-      <Paper elevation={3} />
-      <Paper elevation={3} />
+    <div>
+        <Grid container className={classes.root} spacing={4}>
+          <Grid item={12}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className={classes.media}
+                image={pic}
+                title="Contemplative Reptile"
+                style={myStyles.img}
+              />
+            </CardActionArea>
+          </Card>
+          </Grid>
+          <Grid item={12}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className={classes.media}
+                image={pic}
+                title="Contemplative Reptile"
+                style={myStyles.img}
+              />
+            </CardActionArea>
+          </Card>
+          </Grid>
+          <Grid item={12}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className={classes.media}
+                image={pic}
+                title="Contemplative Reptile"
+                style={myStyles.img}
+              />
+            </CardActionArea>
+          </Card>
+          </Grid>
+          <Grid item={12}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className={classes.media}
+                image={pic}
+                title="Contemplative Reptile"
+                style={myStyles.img}
+              />
+            </CardActionArea>
+          </Card>
+          </Grid>
+          <Grid item={12}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className={classes.media}
+                image={pic}
+                title="Contemplative Reptile"
+                style={myStyles.img}
+              />
+            </CardActionArea>
+          </Card>
+          </Grid>
+        </Grid>
     </div>
-  );
-}
+      )
+    }
