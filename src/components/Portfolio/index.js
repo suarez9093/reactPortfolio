98938c5ac031e2b0813564f54360e4fa0f46b89d
Alex <suarez9093@hotmail.com>
2019-12-29 20:51:25 -0700

    added portfolio images

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-// import Button from '@material-ui/core/Button';
-// import Typography from '@material-ui/core/Typography';
-import pic from "./img/dbz.png"
+import pic from "./img/dbz.png";
+import dbzThumbnail from "./img/dbzThumbnail.png"
+import toyStory from "./img/toyStory.jpg";
+import realEstate from "./img/realEstate.jpg";
+import youTube from "./img/youtubeClone.jpg";
 import Grid from '@material-ui/core/Grid';
 
 
@@ -25,9 +25,9 @@ const useStyles = makeStyles({
 
 const myStyles = {
   img: {
-    // maxWidth: 100,
-    // maxHeight: 100,
-    backgroundPosition: 50
+    width: 140,
+    height: 200,
+ 
   }
 }
 export default function Portfolio() {
@@ -35,73 +35,76 @@ export default function Portfolio() {
 
   return (
     <div>
-        <Grid container className={classes.root} spacing={4}>
-          <Grid item={12}>
+      <Grid container className={classes.root} spacing={4}>
+        <Grid item={12}>
           <Card className={classes.card}>
             <CardActionArea>
               <CardMedia
                 component="img"
                 className={classes.media}
-                image={pic}
-                title="Contemplative Reptile"
+                image={dbzThumbnail}
+                title="Dragon Ball Z Hangman"
                 style={myStyles.img}
+                onClick={() => {
+                 window.location = window.location.href="www.google.com"
+                }}
               />
             </CardActionArea>
           </Card>
-          </Grid>
-          <Grid item={12}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                className={classes.media}
-                image={pic}
-                title="Contemplative Reptile"
-                style={myStyles.img}
-              />
-            </CardActionArea>
-          </Card>
-          </Grid>
-          <Grid item={12}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                className={classes.media}
-                image={pic}
-                title="Contemplative Reptile"
-                style={myStyles.img}
-              />
-            </CardActionArea>
-          </Card>
-          </Grid>
-          <Grid item={12}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                className={classes.media}
-                image={pic}
-                title="Contemplative Reptile"
-                style={myStyles.img}
-              />
-            </CardActionArea>
-          </Card>
-          </Grid>
-          <Grid item={12}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                className={classes.media}
-                image={pic}
-                title="Contemplative Reptile"
-                style={myStyles.img}
-              />
-            </CardActionArea>
-          </Card>
-          </Grid>
         </Grid>
+        <Grid item={12}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className={classes.media}
+                image={realEstate}
+                title="Real Estate App"
+                style={myStyles.img}
+              />
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item={12}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className={classes.media}
+                image={toyStory}
+                title="Toy Story"
+                style={myStyles.img}
+              />
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item={12}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className={classes.media}
+                image={youTube}
+                title="You Tube Clone"
+                style={myStyles.img}
+              />
+            </CardActionArea>
+          </Card>
+        </Grid>
+        {/* <Grid item={12}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className={classes.media}
+                image={pic}
+                title="Contemplative Reptile"
+                style={myStyles.img}
+              />
+            </CardActionArea>
+          </Card>
+        </Grid> */}
+      </Grid>
     </div>
-      )
-    }
+  )
+}
