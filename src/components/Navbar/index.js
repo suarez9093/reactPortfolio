82c5c228a7a08670520d74708ca57dd741cfe0c4
Alex <suarez9093@hotmail.com>
2020-01-22 20:07:33 -0700

    navbar finished. Didn't use CSS grid. Might trip me up in the future but styled for now

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,13 +9,21 @@ function NavbarComponent() {
 
   return (
     <div>
-      <nav id="sideBar">
+      <nav>
         <ul>
           <li>
-            <FontAwesomeIcon icon={faGithubSquare} className="icons" />
-            <FontAwesomeIcon icon={faLinkedin} className="icons" />
-            <FontAwesomeIcon icon={faEnvelope} className="icons" />
-            <FontAwesomeIcon icon={faMobileAlt} className="icons" />
+            <a href="http://www.github.com">
+              <FontAwesomeIcon icon={faGithubSquare} className="icons" />
+            </a>
+            <a href="http://www.linkedin.com">
+              <FontAwesomeIcon icon={faLinkedin} className="icons" />
+            </a>
+            <a href="mailto:suarez9093@hotmail.com">
+              <FontAwesomeIcon icon={faEnvelope} className="icons" />
+            </a>
+            <a href="tel:720-240-6833">
+              <FontAwesomeIcon icon={faMobileAlt} id="phone" className="icons" />
+            </a>
           </li>
         </ul>
       </nav>
