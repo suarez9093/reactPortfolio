934e96ff3dcaeed5ba881f8584518e1b1cef5aeb
Alex <suarez9093@hotmail.com>
2020-01-25 14:02:29 -0700

    header section done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, PropTypes } from 'react';
-import NavbarComponent from "./components/Navbar";
 import {container} from "./css/app.module.scss";
 import "./css/app.css"
 import Header from "./components/Header";
@@ -13,44 +12,14 @@ import { Container, Row, Col } from 'reactstrap';
 function App() {
   
   return (
-    
-    <Container className={container} fluid={true}>
-
-    
-      <Row>
-        <Col>
+    <div>
           <Header />
-        </Col>
-      </Row>
       <h1>Portfolio</h1>
       <h1>About</h1>
       <h1>Footer</h1>
-      {/* <Row>
-        <Col xs="1">
-        <NavbarComponent />
-        </Col>
-        <Col>
-        <Portfolio xs="11">
-        {card.map(project =>
-          
-          <PortfolioCard
-          id={project.id}
-          name={project.name}
-          description={project.description}
-          link={project.link}
-          github={project.github}
-          image={project.image}
-          />
-          )}
-          </Portfolio>
-          </Col>
-          </Row>
-          <Row>
-          <Col>
-          <Footer />
-          </Col>
-        </Row> */}
-    </Container>
+      </div>
+      
+   
 
 
 
