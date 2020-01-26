1e48cf8e4a1c5973989cd4152bab73cd35da80ac
Alex <suarez9093@hotmail.com>
2020-01-26 01:06:45 -0700

    working on shuffle items. Going to try to implement react-flip-move

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,13 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { portfolioSection, section, heading, myWorkColor, portfolioFilter, current, portfolioLinks } from "./css/portfolio.module.scss";
+import { portfolioSection, section, heading, myWorkColor, portfolioFilter, current, portfolioLinks, projectContainer, thumbnail } from "./css/portfolio.module.scss";
 import { Container, Row, Col } from "reactstrap";
+import one from "./img/dbz.png";
+import two from "./img/temp/2.jpg";
+import three from "./img/temp/3.jpg";
+import four from "./img/temp/4.jpg";
+import five from "./img/temp/5.jpg";
+import six from "./img/temp/6.jpg";
+import seven from "./img/temp/7.jpg";
+import eight from "./img/temp/8.jpg";
+import nine from "./img/temp/9.jpg";
+import ten from "./img/temp/10.jpg";
 
 function Portfolio(props) {
 
   return (
     <div className={`${portfolioSection} ${section}`}>
-
       <Container >
         <Row>
           <Col xs="4">
@@ -30,9 +39,17 @@ function Portfolio(props) {
               </a>
             </div>
           </Col>
-
         </Row>
       </Container>
+      <Container fluid = {true}>
+        <div className={projectContainer}>contaier
+        <img className={thumbnail} src={two} alt="us"/>
+        
+        </div>
+
+      </Container>
+
+
     </div>
 
   )
