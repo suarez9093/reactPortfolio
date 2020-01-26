8f6105b62f4f793e4409db2ec9ad74fc7fd66b89
Alex <suarez9093@hotmail.com>
2020-01-25 23:49:52 -0700

    links done

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,13 +1,38 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import {portfolioSection, section} from "./css/portfolio.module.scss";
+import { portfolioSection, section, heading, myWorkColor, portfolioFilter, current, portfolioLinks } from "./css/portfolio.module.scss";
 import { Container, Row, Col } from "reactstrap";
 
 function Portfolio(props) {
- 
+
   return (
     <div className={`${portfolioSection} ${section}`}>
 
-      <Container></Container>
+      <Container >
+        <Row>
+          <Col xs="4">
+            <div className={heading}>
+              <h3><b>Portfolio</b></h3>
+              <h6 className={myWorkColor}><b>My work</b></h6>
+
+            </div>
+          </Col>
+          <Col xs="8">
+            <div className={portfolioFilter}>
+              <a href="#" data-filter="*" className={`${current} ${portfolioLinks}`}>
+                <b>All</b>
+              </a>
+              <a href="#" className={portfolioLinks} data-filter=".react">
+                <b>React</b>
+              </a>
+              <a href="#" className={portfolioLinks} data-filter=".full-stack">
+                <b>Full Stack</b>
+              </a>
+            </div>
+          </Col>
+
+        </Row>
+      </Container>
     </div>
 
   )
