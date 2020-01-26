7ec6f940130c19395f30a5bd046d4f8d38d52772
Alex <suarez9093@hotmail.com>
2020-01-26 03:52:10 -0700

    portfolio section done

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,17 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, {useState} from 'react';
-import { portfolioSection, section, heading, myWorkColor, portfolioFilter, current, portfolioLinks, projectContainer, flipBox, flipBoxInner, flipBoxFront, flipBoxBack, flipBoxTwo, flipBoxTwoInner, flipBoxTwoFront, flipBoxTwoBack, flipBoxThree, flipBoxThreeInner, flipBoxThreeFront, flipBoxThreeBack} from "./css/portfolio.module.scss";
+import React, { useState } from 'react';
+import { portfolioSection, section, heading, myWorkColor, portfolioFilter, current, portfolioLinks, projectContainer, flipBox, flipBoxInner, flipBoxFront, flipBoxBack, flipBoxTwo, flipBoxTwoInner, flipBoxTwoFront, flipBoxTwoBack, flipBoxThree, flipBoxThreeInner, flipBoxThreeFront, flipBoxThreeBack , flipBoxFour, flipBoxFourInner, flipBoxFourFront, flipBoxFourBack , flipBoxFive, flipBoxFiveInner, flipBoxFiveFront, flipBoxFiveBack } from "./css/portfolio.module.scss";
 import { Container, Row, Col } from "reactstrap";
-import one from "./img/dbz.png";
-import two from "./img/temp/2.jpg";
-import three from "./img/temp/3.jpg";
-import four from "./img/temp/4.jpg";
-import five from "./img/temp/5.jpg";
-import six from "./img/temp/6.jpg";
-import seven from "./img/temp/7.jpg";
-import eight from "./img/temp/8.jpg";
-import nine from "./img/temp/9.jpg";
-import ten from "./img/temp/10.jpg";
+import realEstate from "./img/realEstate.jpg"
 
 function Portfolio(props) {
   const [fullScreen, setFullScreen] = useState(false)
@@ -28,7 +19,7 @@ function Portfolio(props) {
 
             </div>
           </Col>
-          <Col xs="8">
+          {/* <Col xs="8">
             <div className={portfolioFilter}>
               <a href="#" data-filter="*" className={`${current} ${portfolioLinks}`}>
                 <b>All</b>
@@ -40,97 +31,97 @@ function Portfolio(props) {
                 <b>Full Stack</b>
               </a>
             </div>
-          </Col>
+          </Col> */}
         </Row>
       </Container>
       <Container fluid={true}>
         <Row>
-        
-        <section>
-            <div className={flipBoxTwo}>
-                <div className={flipBoxTwoInner}>
-                    <div className={flipBoxTwoFront}>
-                        {/* <img src={props.image} alt={props.name} style={{ width: "300px", height: "200px" }} /> */}
-                    </div>
-                    <div className={flipBoxTwoBack}>
-                        <h2>{props.name}</h2>
-                        <a href={props.link}>Two</a>
-                        <br />
-                        <a href={props.github}>Link to Github</a>
-                        <p>{props.description}</p>
-                    </div>
-                </div>
-            </div>
-        </section>
 
-        <section>
+          <section>
             <div className={flipBoxTwo}>
-                <div className={flipBoxTwoInner}>
-                    <div className={flipBoxTwoFront}>
-                        {/* <img src={props.image} alt={props.name} style={{ width: "300px", height: "200px" }} /> */}
-                    </div>
-                    <div className={flipBoxTwoBack}>
-                        <h2>{props.name}</h2>
-                        <a href={props.link}>Two</a>
-                        <br />
-                        <a href={props.github}>Link to Github</a>
-                        <p>{props.description}</p>
-                    </div>
+              <div className={flipBoxTwoInner}>
+                <div className={flipBoxTwoFront}>
+                  {/* <img src={props.image} alt={props.name} style={{ width: "300px", height: "200px" }} /> */}
                 </div>
+                <div className={flipBoxTwoBack}>
+                  <h2>{props.name}</h2>
+                  <a href="https://project-three-real-estate.herokuapp.com/">Link To Real Estate App</a>
+                  <br />
+                  <a href="https://github.com/suarez9093/real-estate">Link to Github</a>
+                  <p>description</p>
+                </div>
+              </div>
             </div>
-        </section>
+          </section>
 
-        <section>
-        <div className={flipBox}>
-                <div className={flipBoxInner}>
-                    <div className={flipBoxFront}>
-                        {/* <img src={props.image} alt={props.name} style={{ width: "300px", height: "200px" }} /> */}
-                    </div>
-                    <div className={flipBoxBack}>
-                        <h2>{props.name}</h2>
-                        <a href={props.link}>One</a>
-                        <br />
-                        <a href={props.github}>Link to Github</a>
-                        <p>{props.description}</p>
-                    </div>
+          <section>
+            <div className={flipBoxFive}>
+              <div className={flipBoxFiveInner}>
+                <div className={flipBoxFiveFront}>
+                  {/* <img src={props.image} alt={props.name} style={{ width: "300px", height: "200px" }} /> */}
                 </div>
+                <div className={flipBoxFiveBack}>
+                  <h2>{props.name}</h2>
+                  <a href={props.link}>Link to DragonBall Z Hangman</a>
+                  <br />
+                  <a href={props.github}>Link to Github</a>
+                  <p>{props.description}</p>
+                </div>
+              </div>
             </div>
-        </section>
-        <section>
+          </section>
+
+          <section>
+            <div className={flipBox}>
+              <div className={flipBoxInner}>
+                <div className={flipBoxFront}>
+                  {/* <img src={props.image} alt={props.name} style={{ width: "300px", height: "200px" }} /> */}
+                </div>
+                <div className={flipBoxBack}>
+                  <h2>{props.name}</h2>
+                  <a href={props.link}>Link to YouTube Clone</a>
+                  <br />
+                  <a href={props.github}>Link to Github</a>
+                  <p>{props.description}</p>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section>
             <div className={flipBoxThree}>
-                <div className={flipBoxThreeInner}>
-                    <div className={flipBoxFront}>
-                        {/* <img src={props.image} alt={props.name} style={{ width: "300px", height: "200px" }} /> */}
-                    </div>
-                    <div className={flipBoxThreeBack}>
-                        <h2>{props.name}</h2>
-                        <a href={props.link}>3</a>
-                        <br />
-                        <a href={props.github}>Link to Github</a>
-                        <p>{props.description}</p>
-                    </div>
+              <div className={flipBoxThreeInner}>
+                <div className={flipBoxThreeFront}>
+                  {/* <img src={props.image} alt={props.name} style={{ width: "300px", height: "200px" }} /> */}
                 </div>
-            </div>
-        </section>
-        <section>
-            <div className={flipBoxThree}>
-                <div className={flipBoxThreeInner}>
-                    <div className={flipBoxFront}>
-                        {/* <img src={props.image} alt={props.name} style={{ width: "300px", height: "200px" }} /> */}
-                    </div>
-                    <div className={flipBoxThreeBack}>
-                        <h2>{props.name}</h2>
-                        <a href={props.link}>3</a>
-                        <br />
-                        <a href={props.github}>Link to Github</a>
-                        <p>{props.description}</p>
-                    </div>
+                <div className={flipBoxThreeBack}>
+                  <h2>{props.name}</h2>
+                  <a href={props.link}>Link to Speed Typing Game</a>
+                  <br />
+                  <a href={props.github}>Link to Github</a>
+                  <p>{props.description}</p>
                 </div>
+              </div>
             </div>
-        </section>
-        
-      
-        
+          </section>
+          <section>
+            <div className={flipBoxFour}>
+              <div className={flipBoxFourInner}>
+                <div className={flipBoxFourFront}>
+                  {/* <img src={props.image} alt={props.name} style={{ width: "300px", height: "200px" }} /> */}
+                </div>
+                <div className={flipBoxFourBack}>
+                  <h2>{props.name}</h2>
+                  <a href={props.link}>Link to Toy Story Game</a>
+                  <br />
+                  <a href={props.github}>Link to Github</a>
+                  <p>{props.description}</p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
+
         </Row>
 
       </Container>
