759d1adcbcaac883a301b39a7441662171bea3e9
suarez9093 <suarez9093@hotmail.com>
2021-02-05 15:13:22 -0500

    added new resume, compressed images and did lazy loading

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,36 +1,59 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
-import { heading, myWorkColor, font } from "./css/about.module.scss";
-import mern from "./img/mern.png";
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { heading, myWorkColor, font } from './css/about.module.scss';
+import mern from './img/mern.png';
 
 function About() {
   return (
-    <Container >
+    <Container>
       <Row>
-        <Col xs="4">
+        <Col xs='4'>
           <div className={heading}>
-            <h3><b>About Me</b></h3>
-            <h6 className={myWorkColor}><b>Professional Path</b></h6>
-
+            <h3>
+              <b>About Me</b>
+            </h3>
+            <h6 className={myWorkColor}>
+              <b>Professional Path</b>
+            </h6>
           </div>
         </Col>
-        <Col xs="8">
-          <h6 className={font}>Results-driven Full Stack Web Developer applying strong technical skills toward building functional applications. Graduated with a certification in Full Stack Web Development from the University of Denver. I have built full-stack web applications and acquired strong knowledge in React, MYSQL, Express, Node.js, and JavaScript. When I venture down a path that I am passionate about, I focus all of my time and energy on achieving my goals. I’ve toured as a professional musician and have been a sponsored athlete competing in competitions. In times of adversity, I am the calmest. In fast-paced situations, I rely on logic to persevere when an application isn’t working as planned. I adapt quickly and can communicate effectively; this attribute was crucial to the success of working with a team to deliver projects with strict deadlines. I focus on small wins that make up the big picture which motivates me, in turn, to keep pursuing my passion. I love being given an obstacle; it allows me to use my technical abilities, past experiences, and strengths to push through and build something even more intuitive and creative than I’ve done in the past. </h6>
+        <Col xs='8'>
+          <h6 className={font}>
+            Results-driven Full Stack Web Developer applying strong technical
+            skills toward building functional applications. Graduated with a
+            certification in Full Stack Web Development from the University of
+            Denver. I have built full-stack web applications and acquired strong
+            knowledge in React, MYSQL, Express, Node.js, and JavaScript. When I
+            venture down a path that I am passionate about, I focus all of my
+            time and energy on achieving my goals. I’ve toured as a professional
+            musician and have been a sponsored athlete competing in
+            competitions. In times of adversity, I am the calmest. In fast-paced
+            situations, I rely on logic to persevere when an application isn’t
+            working as planned. I adapt quickly and can communicate effectively;
+            this attribute was crucial to the success of working with a team to
+            deliver projects with strict deadlines. I focus on small wins that
+            make up the big picture which motivates me, in turn, to keep
+            pursuing my passion. I love being given an obstacle; it allows me to
+            use my technical abilities, past experiences, and strengths to push
+            through and build something even more intuitive and creative than
+            I’ve done in the past.{' '}
+          </h6>
         </Col>
       </Row>
       <br />
       <br />
       <Row>
-
-        <Col xs="4"></Col>
-        <Col xs="8">
-          <img src={mern} className="img-fluid" alt="Responsive-img" />
-
+        <Col xs='4'></Col>
+        <Col xs='8'>
+          <img
+            loading='lazy'
+            src={mern}
+            className='img-fluid'
+            alt='Responsive-img'
+          />
         </Col>
-
       </Row>
     </Container>
-  )
-
-};
+  );
+}
 export default About;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,53 +1,71 @@
 import React from 'react';
-import { header, intro, profileImage, portfolioImage, fontYellow, information, socialIcons, fontAwesomeIcon } from "./css/header.module.scss";
-import "./css/header.css"
-import headshot from "./img/headshot.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons"
-import { faPaperclip } from "@fortawesome/free-solid-svg-icons"
-import resume from "./resume/alex_suarez_resume.pdf";
+import {
+  header,
+  intro,
+  profileImage,
+  portfolioImage,
+  fontYellow,
+  information,
+  socialIcons,
+  fontAwesomeIcon,
+} from './css/header.module.scss';
+import './css/header.css';
+import headshot from './img/headshot.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGithub,
+  faLinkedin,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import { faPaperclip } from '@fortawesome/free-solid-svg-icons';
+import resume from './resume/alex_suarez_resume.pdf';
 
 function Header() {
   return (
     <header className={header}>
       <div className={intro}>
         <div className={profileImage}>
-          <img id={portfolioImage} src={headshot} alt="headshot" />
+          <img
+            loading='lazy'
+            id={portfolioImage}
+            src={headshot}
+            alt='headshot'
+          />
         </div>
         <h2>Alex Suarez</h2>
         <h4 className={fontYellow}>Full Stack Developer</h4>
         <ul className={information}>
           <li>
             <b>Phone: </b>
-            <a href="tel:720-240-6833">720-240-6833</a>
+            <a href='tel:720-240-6833'>720-240-6833</a>
           </li>
           <li>
             <b>Email: </b>
-            <a href="mailto:suarez9093@hotmail.com">
-              suarez9093@hotmail.com
-                    </a>
+            <a href='mailto:suarez9093@hotmail.com'>suarez9093@hotmail.com</a>
           </li>
-          <li>
-          </li>
+          <li></li>
         </ul>
         <ul className={socialIcons}>
           <li className={socialIcons}>
-            <a title="Github" href="https://github.com/suarez9093">
+            <a title='Github' href='https://github.com/suarez9093'>
               <FontAwesomeIcon className={fontAwesomeIcon} icon={faGithub} />
             </a>
           </li>
           <li className={socialIcons}>
-            <a title="Instagram" href="https://www.instagram.com/suarez9093/">
+            <a title='Instagram' href='https://www.instagram.com/suarez9093/'>
               <FontAwesomeIcon className={fontAwesomeIcon} icon={faInstagram} />
             </a>
           </li>
           <li className={socialIcons}>
-            <a title="LinkedIn" href="https://www.linkedin.com/in/alexsuarez9093/">
+            <a
+              title='LinkedIn'
+              href='https://www.linkedin.com/in/alexsuarez9093/'
+            >
               <FontAwesomeIcon className={fontAwesomeIcon} icon={faLinkedin} />
             </a>
           </li>
           <li className={socialIcons}>
-            <a title="Resume" href={resume}>
+            <a title='Resume' href={resume}>
               <FontAwesomeIcon className={fontAwesomeIcon} icon={faPaperclip} />
             </a>
           </li>
@@ -57,4 +75,4 @@ function Header() {
   );
 }
 
-export default Header
+export default Header;
